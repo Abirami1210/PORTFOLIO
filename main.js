e3343d35e0ae0bce81dbe9b7f48f389f7a47c368
Abirami1210 <sabirami5488@gmail.com>
2024-11-09 06:26:13 +0530

    Add files via upload
    yes

---
+++ main.js
@@ -0,0 +1,45 @@
+// (function($){
+//     "use strict"
+// $(window).scroll(function(){
+//     if($(this).scrollTop() > 200){
+//         $('.navbar').fadeIn('slow').css('display','flex');
+//     }
+//     else{
+//         $('.navbar').fadeOut('slow').css('display','none');
+//     }
+// });
+
+
+
+
+    // if($('.typed-text-output').length==1){
+    //     var typed_strings = $('.typed-text').text();
+    //     var typed  = new Typed('.typed-text-output',{
+    //  strings:typed_strings.split(', '),
+    //         typeSpeed:100,
+    //         backSpeed:20,
+    //         smartBackspace:false,
+    //         loop:true
+    //     });
+
+
+    // var portfolioIsotope = $('.portfolio-container').isotope({itemSelector: '.portfolio-item',
+    //     layoutMode:'fitRows'});
+    //    $('#portfolio-flters li').on('click', function(){
+    //      $("portfolio-filters li").removeClass('active');
+    //      $(this).addClass('active');
+     
+    //      portfolioIsotope.isotope({filter:$(this).data('filter')});
+    //    })
+
+    var typed = new Typed('#typed',{
+        strings:['UI Developer', 'Front-End Developer','Web Developer'],
+    
+        typeSpeed:100,
+      });
+    
+// skill
+
+
+
+    (jQuery);
